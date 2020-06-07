a8ccaed64fbc67a743c91d251366a5896d81a4f8
Shumyla Ansari <shumyla.fahd@hotmail.com>
2020-06-07 17:56:43 +0100

    create basic react app

---
+++ src/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './hello.css'
+
+function Hello({firstName}) {
+return <p className ="myname">This is para in Hello tag : {firstName}</p>
+}
+
+export default Hello;
+ 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div><App name="Shumyla Ansari" age={25}/></div>,
   document.getElementById('root')
 );
 
